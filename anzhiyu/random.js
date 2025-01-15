8dba9cbc76d049e30594768e9cca82bc53a72c12
Tanpinsary <1925352047@qq.com>
2025-01-15 17:20:01 +0800

    Site updated: 2025-01-15 17:20:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/08/1月7日之事/"];function toRandomPost(){
+var posts=["2025/01/08/1月7日之事/","2025/01/01/数学分析（上）复习提纲/","2024/12/31/通信概论复习/","2025/01/03/线性代数复习提纲/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
